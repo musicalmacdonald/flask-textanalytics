5d76f56211768648d7be94311e9b8ad14c7249cc
John Funk <email@johnfunk.com>
2018-02-18 13:28:54 -0700

    Part 2 update

--- library/hello_world.py
+++ library/hello_world.py
@@ -1,9 +1,18 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
+
+
+@app.route('/test')
+def mainpage():
+    name = 'Matie'
+    return render_template('index.html', name=name)
+
 
 @app.route('/')
 def hello_world():
     return 'hello world'
+
+
 
--- run-app.py
+++ run-app.py
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     host = os.environ.get('IP', '127.0.0.1')
     port = int(os.environ.get('PORT', 8080))
 app.run(host=host, port=port)
+
